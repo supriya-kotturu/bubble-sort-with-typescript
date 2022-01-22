3a3ec6df93d22d1414b998eecc20311e9730e623
supriya-kotturu <supriyakotturu@pm.me>
2022-01-22 20:43:08 +0530

    refactor and add Sorter Class

--- build/index.js
+++ build/index.js
@@ -1,25 +1,13 @@
 "use strict";
-class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
-    sort() {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    this.collection[j] = this.collection[j] + this.collection[j + 1];
-                    this.collection[j + 1] = this.collection[j] - this.collection[j + 1];
-                    this.collection[j] = this.collection[j] - this.collection[j + 1];
-                    //   const leftValue = this.collection[j];
-                    //   this.collection[j] = this.collection[j + 1];
-                    //   this.collection[j + 1] = leftValue;
-                }
-            }
-        }
-        console.log('here : ', this.collection);
-    }
-}
-const sorter = new Sorter([0, -4, 5, 2]);
-sorter.sort();
-console.log(sorter.collection);
+Object.defineProperty(exports, "__esModule", { value: true });
+const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
+const Sorter_1 = require("./Sorter");
+const numbersCollection = new NumbersCollection_1.NumbersCollection([0, -4, 5, 2]);
+const NumSorter = new Sorter_1.Sorter(numbersCollection);
+NumSorter.sort();
+console.log(numbersCollection.data);
+const charactersCollection = new CharactersCollection_1.CharactersCollection('freaky friday');
+const CharSorter = new Sorter_1.Sorter(charactersCollection);
+CharSorter.sort();
+console.log(charactersCollection.data);
